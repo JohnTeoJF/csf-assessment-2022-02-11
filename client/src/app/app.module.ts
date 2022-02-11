37df15dd7304fbed5be5b1841d704d4642af6880
JohnTeoJF <JohnTeoJF+git@gmail.com>
2022-02-11 09:45:28 +0800

    Task 2
    Generate 3 Angular Components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './components/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe-detail.component';
+import { RecipeAddComponent } from './components/recipe-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeAddComponent
   ],
   imports: [
     BrowserModule
